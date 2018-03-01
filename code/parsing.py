27c7b03331ac0f496752c588aba7f935482dfbc5
Batary <bat_jarry@hotmail.fr>
2018-03-01 20:52:04 +0100

    debut writer

--- code/parsing.py
+++ code/parsing.py
@@ -16,12 +16,20 @@ def read_file(file):
 		l = lines[a]
 		for i in range(len(l)):
 			l[i] = int(l[i])
-		lines[a] = [(l[0], l[1]), (l[2], l[3]), l[4], l[5], abs(l[0] - l[2]) + abs(l[1] - l[3]) ]
+		lines[a] = [(l[0], l[1]), (l[2], l[3]), l[4], l[5], abs(l[0] - l[2]) + abs(l[1] - l[3]), a ]
 	
 	# print(lines)
 	
 	return tt[0], tt[1], tt[2], tt[3], tt[4], tt[5], lines
 	
 # 3 4 2 3 2 10
-# rides[0] = [(0, 0), (1, 3), 2, 9, 4]
+# rides[0] = [(0, 0), (1, 3), 2, 9, 4, 0]
 rows, cols, nbVehicles, maxRides, bonus, maxSteps, rides  = read_file("../instances/a_example.in")
+print(rides)
+
+
+
+def write_output(vehicles, output_file):
+	out = open(output_file, "w")
+	for v in range(len(vehicles)):
+		out.write(str(v) + " " +  + "\n")
