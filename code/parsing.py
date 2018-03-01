8e7059d02251d58d1bcc0a4df890dbf52c7f70f7
Batary <bat_jarry@hotmail.fr>
2018-03-01 20:23:55 +0100

    parser fonctionnel

---
+++ code/parsing.py
@@ -0,0 +1,27 @@
+
+# parser
+
+
+def read_file(file):
+	f = open(file, "r")
+	lines = f.read().split("\n")
+	t = lines.pop(0).split(" ")
+	tt = [int(x) for x in t]
+	
+	if(len(lines[-1]) < 2) : lines.pop()
+	
+	lines = [x.split(" ") for x in lines]
+	
+	for a in range(len(lines)):
+		l = lines[a]
+		for i in range(len(l)):
+			l[i] = int(l[i])
+		lines[a] = [(l[0], l[1]), (l[2], l[3]), l[4], l[5], abs(l[0] - l[2]) + abs(l[1] - l[3]) ]
+	
+	# print(lines)
+	
+	return tt[0], tt[1], tt[2], tt[3], tt[4], tt[5], lines
+	
+# 3 4 2 3 2 10
+# rides[0] = [(0, 0), (1, 3), 2, 9, 4]
+rows, cols, nbVehicles, maxRides, bonus, maxSteps, rides  = read_file("../instances/a_example.in")
